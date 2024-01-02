ab98abecb3e161ea01040bed33b824c692cb6b38
Teron Bullock <teron.bullock@gmail.com>
2024-01-01 21:46:38 -0500

    update nav toggle script

--- src/js/nav-toggle-script.js
+++ src/js/nav-toggle-script.js
@@ -1,29 +1,17 @@
 // Get the navbar
-let nav = document.querySelector('[data-js="nav"]');
+const nav = document.querySelector('[data-js="nav"]');
+const navToggle = document.querySelector('[data-js="btn-mobile-toggle"]');
 
-/* Open when someone clicks on #nav-toggle-btn */
+
 function toggleNav() { 
   if (nav.classList.contains("open") ) {
-    document.getElementById("js-btn-mobile-toggle").classList.remove("change");
+    navToggle.classList.remove("change");
     nav.classList.remove("open");
   } else  {
     nav.classList.add("open");
-    document.getElementById("js-btn-mobile-toggle").classList.add("change");
+    navToggle.classList.add("change");
   }
 }
 
-/* Close Overlay */
-function closeNav() {
-  document.getElementById("js-btn-mobile-toggle").classList.remove("change");
-  if (window.screen.width <= 700) {
-    nav.removeAttribute("style");
-    
-  } 
 
-}
-
-
-/* Mobile Menu Toogle */
-function navButton(x) {
-  x.classList.toggle("change");
-}
+navToggle.addEventListener("click", toggleNav );
