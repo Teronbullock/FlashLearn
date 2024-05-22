ad1e88a0444d2b2e19347ede4aaf4674b888e4d6
Teron Bullock <teron.bullock@gmail.com>
2024-05-22 12:32:36 -0400

    update card and index style

--- app.js
+++ app.js
@@ -97,5 +97,5 @@ app.use((err, req, res, next) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Express app listening on port ${port}`);
+  console.log(`Express app listening on http://localhost:${port}`);
 });
